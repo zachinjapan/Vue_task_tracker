d50bdcecfebe5f0b650a45e2cbe7dfaafccef100
Zach Stone <zachinjapan888@gmail.com>
2022-06-03 19:05:45 -0400

    added css styles and a few basic components

---
+++ src/components/AppButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "AppButton",
+  props: {
+    text: {
+      type: String,
+      default: "Button Text N/A",
+    },
+    color: {
+      type: String,
+      default: "blue",
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("Button clicked!");
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <AppButton text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import AppButton from "./AppButton.vue";
+
+export default {
+  name: "AppHeader",
+  components: {
+    AppButton,
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: "Header Prop N/A",
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
